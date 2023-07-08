b9654bcadbc727ed8c6dff21cdf94376ce85c11a
PierreMaze <promazelaygue@gmail.com>
2023-07-08 15:27:15 +0200

    Update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,10 +19,21 @@ const Home = () => {
 
   return (
     <div>
-      <h2>Hello!</h2>
+      <h2>Hello World!</h2>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>{user.fullname}</li>
+          <li key={user.id}>
+            <div className="box-img">
+              <img src={user.picture_profile} alt="picture" />
+            </div>
+            <h1>Salut! Je m&apos;appelle {user.fullname}</h1>
+            <h2>Tu peux me contacter avec ce mail: {user.email}</h2>
+            <h3>Ou alors par ce numéro: {user.phone}</h3>
+            <h4>je suis une {user.gender}</h4>
+            <h5>
+              Je suis Cheffe de chantier Pays-Urbain dans le {user.employment}
+            </h5>
+          </li>
         ))}
       </ul>
     </div>
