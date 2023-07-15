72e8a0ee3e9c6605d12a83da3f22599ffd21cda3
PierreMaze <promazelaygue@gmail.com>
2023-07-15 15:23:48 +0200

    Update css card

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import CardBody from "./CardBody";
-import CardImage from "./CardIlmg";
+import Footer from "./FooterCard";
+
 const Card = () => {
   const [users, setUsers] = useState([]);
 
@@ -21,18 +21,46 @@ const Card = () => {
   }, []);
 
   return (
-    <div className="box-card">
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <div className="container">
-              <CardImage />
-              <CardBody />
+    <ul className="box-card">
+      {users.map((user) => (
+        <li key={user.id}>
+          <div className="container">
+            <div className="box-main">
+              <div className="box-img">
+                <img src={user.picture_profile} alt="picture" />
+              </div>
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+            <div className="box-secondary">
+              <div className="box-title">
+                <h1>Hello World!</h1>
+                <h2>My name is {user.fullname}</h2>
+              </div>
+              <div className="box-subtitle">
+                <p>
+                  You can contact me by email :
+                  <a href="https://www.linkedin.com/\in\pierremazelaygue">
+                    <strong> {user.email}</strong>
+                  </a>
+                </p>
+                <p>
+                  <strong>Or by phone: </strong>
+                  {user.phone}.
+                </p>
+                <p>
+                  <strong>Gender : </strong>
+                  {user.gender}.
+                </p>
+                <p>
+                  <strong>Job : </strong>
+                  {user.employment}.
+                </p>
+                <Footer />
+              </div>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
