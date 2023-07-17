2930c44dbf67c5bdc4d098544dc18379d8ae8994
PierreMaze <promazelaygue@gmail.com>
2023-07-17 15:48:15 +0200

    Hotfix database

--- backend/database.sql
+++ backend/database.sql
@@ -16,5 +16,9 @@ CREATE TABLE `users` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
 
 INSERT INTO `users` (fullname, email, gender, phone, picture_profile, is_admin, employment)
-VALUES ("Sassy", "Sassy@exemple.com", "Women", '0465432107', "https://avatars.dicebear.com/api/micah/Sassy.svg", false, "Architect");
--- ("John Doe", "john.doe@example.com", "Men", '0123456789', "https://avatars.dicebear.com/api/micah/JohnDoe.svg", false, "Engineer"), ("Alice Smith", "alice.smith@example.com", "Women", '0987654321', "https://avatars.dicebear.com/api/micah/AliceSmith.svg", false, "Doctor"), ("Michael Johnson", "michael.johnson@example.com", "Men", '0765432109', "https://avatars.dicebear.com/api/micah/MichaelJohnson.svg", false, "Teacher"), ("Emily Brown", "emily.brown@example.com", "Women", '0654321098', "https://avatars.dicebear.com/api/micah/EmilyBrown.svg", false, "Accountant"), (fullname, email, gender, phone, picture_profile, is_admin, employment), ("David Wilson", "david.wilson@example.com", "Men", '0543210987', "https://avatars.dicebear.com/api/micah/DavidWilson.svg", false, "Lawyer");
+VALUES ("Sassy", "Sassy@exemple.com", "Women", '0465432107', "https://avatars.dicebear.com/api/micah/Sassy.svg", false, "Architect"),
+("John Doe", "john.doe@example.com", "Men", '0123456789', "https://avatars.dicebear.com/api/micah/JohnDoe.svg", false, "Engineer"), 
+("Alice Smith", "alice.smith@example.com", "Women", '0987654321', "https://avatars.dicebear.com/api/micah/AliceSmith.svg", false, "Doctor"), 
+("Sophie Johnson", "sophie.johnson@example.com", "Men", '0765432109', "https://avatars.dicebear.com/api/micah/MichaelJohnson.svg", false, "Teacher"), 
+("David Brown", "david.brown@example.com", "Women", '0654321098', "https://avatars.dicebear.com/api/micah/EmilyBrown.svg", false, "Accountant"), (fullname, email, gender, phone, picture_profile, is_admin, employment), 
+("Emily Wilson", "emily.wilson@example.com", "Men", '0543210987', "https://avatars.dicebear.com/api/micah/DavidWilson.svg", false, "Lawyer");
